228fbfdcb457d2d65c06dc9295fea5b127bde700
prabhu797 <prabhudesai007@gmail.com>
2023-07-22 17:44:12 +0530

    Updated CORS.

--- src/main/java/com/student/controller/CrossOriginRequests.java
+++ src/main/java/com/student/controller/CrossOriginRequests.java
@@ -5,14 +5,18 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
-public class CrossOriginRequests implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
-    }
+public class CrossOriginRequests {
+	
 }
+
+//@Configuration
+//@EnableWebMvc
+//public class CrossOriginRequests implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("*");
+//    }
+//}
